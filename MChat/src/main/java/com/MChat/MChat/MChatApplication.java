be38976e3ddbc74d9633c63426deda0e60c08f20
mark <mark.rizkalla1@gmail.com>
2024-09-11 02:27:05 +0300

    Initial commit

---
+++ MChat/src/main/java/com/MChat/MChat/MChatApplication.java
@@ -0,0 +1,13 @@
+package com.MChat.MChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MChatApplication.class, args);
+	}
+
+}
---
+++ MChat/src/test/java/com/MChat/MChat/MChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MChat.MChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
