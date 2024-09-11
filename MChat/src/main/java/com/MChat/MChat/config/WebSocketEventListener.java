639d762b325d9e46b132b284dee9e196162ea777
mena <mena.rizkalla1@gmail.com>
2024-09-12 01:33:54 +0300

    implement web socket listener

--- MChat/src/main/java/com/MChat/MChat/config/WebSocketEventListener.java
+++ MChat/src/main/java/com/MChat/MChat/config/WebSocketEventListener.java
@@ -1,8 +1,13 @@
 package com.MChat.MChat.config;
 
+import com.MChat.MChat.chat.ChatMessage;
+import com.MChat.MChat.chat.MessageType;
+import jakarta.validation.constraints.Null;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -11,10 +16,22 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @Slf4j
 public class WebSocketEventListener {
 
+    private final SimpMessageSendingOperations messageSendingOperations;
+
     @EventListener
     public void handleWebSocketDisconnectListener(
             SessionDisconnectEvent event
     ){
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String userName = (String) headerAccessor.getSessionAttributes().get("username");
 
+        if (userName != null){
+            log.info("User disconnected: {}", userName);
+            var message = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(userName)
+                    .build();
+            messageSendingOperations.convertAndSend("/topic/public",message);
+        }
     }
 }
